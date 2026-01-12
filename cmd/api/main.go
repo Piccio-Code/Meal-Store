149c6b3ce56a892ab6a2e7c04bee91c9a041d21e
PiccioHub <picciohub@gmail.com>
2026-01-12 13:08:16 +0100

    Revert "pgxpool setup"
    This reverts commit 0874a936269cb400caae0a8bb42f3766962042c1.

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,12 +1,8 @@
 package main
 
 import (
-	"context"
-	"encoding/json"
 	"flag"
 	"fmt"
-	"github.com/jackc/pgx/v5/pgxpool"
-	"github.com/joho/godotenv"
 	"log"
 	"net/http"
 	"os"
@@ -16,44 +12,18 @@ import (
 const version = "1.0.0"
 
 type config struct {
-	port       int
-	env        string
-	poolConfig poolConfig
-}
-
-type poolConfig struct {
-	MaxConns          int32         `json:"maxConns,omitempty"`
-	MinConns          int32         `json:"minConns,omitempty"`
-	MaxConnLifetime   time.Duration `json:"maxConnLifetime,omitempty"`
-	MaxConnIdleTime   time.Duration `json:"maxConnIdleTime,omitempty"`
-	HealthCheckPeriod time.Duration `json:"healthCheckPeriod,omitempty"`
+	port int
+	env  string
 }
 
 type application struct {
 	infoLog  *log.Logger
 	errorLog *log.Logger
-	pool     *pgxpool.Pool
 	config   config
 }
 
 func main() {
-	poolConfig := poolConfig{
-		MaxConns:          10,
-		MinConns:          0,
-		MaxConnLifetime:   time.Hour * 1,
-		MaxConnIdleTime:   time.Minute * 30,
-		HealthCheckPeriod: time.Minute,
-	}
-
-	cfg := config{
-		poolConfig: poolConfig,
-	}
-
-	err := godotenv.Load()
-
-	if err != nil {
-		log.Fatal("Error loading .env file")
-	}
+	var cfg config
 
 	flag.IntVar(&cfg.port, "port", 8080, "The port of the backend.")
 	flag.StringVar(&cfg.env, "env", "development", "Environment (development|staging|production)")
@@ -62,25 +32,9 @@ func main() {
 	infoLog := log.New(os.Stdout, "INFO:\t", log.LstdFlags)
 	errorLog := log.New(os.Stderr, "ERROR:\t", log.LstdFlags|log.Lshortfile)
 
-	pool, err := NewDBPool()
-
-	if err != nil {
-		log.Println("Error connecting to the DB")
-		log.Fatal(err)
-	}
-
-	pool.Config().MaxConns = cfg.poolConfig.MaxConns
-	pool.Config().MinConns = cfg.poolConfig.MinConns
-	pool.Config().MaxConnLifetime = cfg.poolConfig.MaxConnLifetime
-	pool.Config().MaxConnIdleTime = cfg.poolConfig.MaxConnIdleTime
-	pool.Config().HealthCheckPeriod = cfg.poolConfig.HealthCheckPeriod
-
-	defer pool.Close()
-
 	app := &application{
 		infoLog:  infoLog,
 		errorLog: errorLog,
-		pool:     pool,
 		config:   cfg,
 	}
 
@@ -94,40 +48,4 @@ func main() {
 
 	app.infoLog.Printf("starting %s server on http://localhost:%d", cfg.env, cfg.port)
 	app.errorLog.Fatal(srv.ListenAndServe())
-}
-
-func NewDBPool() (pool *pgxpool.Pool, err error) {
-	pool, err = pgxpool.New(context.Background(), os.Getenv("DB_DSN"))
-
-	if err != nil {
-		return nil, err
-	}
-
-	err = pool.Ping(context.Background())
-
-	if err != nil {
-		return nil, err
-	}
-
-	return pool, nil
-}
-
-func (p poolConfig) MarshalJSON() ([]byte, error) {
-	type poolConfigOut struct {
-		MaxConns          int32  `json:"maxConns,omitempty"`
-		MinConns          int32  `json:"minConns,omitempty"`
-		MaxConnLifetime   string `json:"maxConnLifetime,omitempty"`
-		MaxConnIdleTime   string `json:"maxConnIdleTime,omitempty"`
-		HealthCheckPeriod string `json:"healthCheckPeriod,omitempty"`
-	}
-
-	js := poolConfigOut{
-		MaxConns:          p.MaxConns,
-		MinConns:          p.MinConns,
-		MaxConnLifetime:   p.MaxConnLifetime.String(),
-		MaxConnIdleTime:   p.MaxConnIdleTime.String(),
-		HealthCheckPeriod: p.HealthCheckPeriod.String(),
-	}
-
-	return json.MarshalIndent(js, "", "\t")
 }
