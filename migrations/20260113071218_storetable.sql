ee659e3a60593eaffbd797779aa7e7d69a0bbc68
PiccioHub <picciohub@gmail.com>
2026-01-13 15:11:51 +0100

    Better names and table name for stores

--- migrations/20260113071218_storetable.sql
+++ migrations/20260113071218_storetable.sql
@@ -1,15 +1,17 @@
 -- +goose Up
 -- +goose StatementBegin
-CREATE TABLE store (
-    ID SERIAL PRIMARY KEY,
-    Name VARCHAR(255) NOT NULL,
-    UserID VARCHAR(255),
-    CreatedAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    FOREIGN KEY (UserID) REFERENCES "user"(id)
+CREATE TABLE stores (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    user_id VARCHAR(255),
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (user_id) REFERENCES "user"(id)
 );
+
+
 -- +goose StatementEnd
 
 -- +goose Down
 -- +goose StatementBegin
-DROP TABLE IF EXISTS store;
+DROP TABLE IF EXISTS stores;
 -- +goose StatementEnd
