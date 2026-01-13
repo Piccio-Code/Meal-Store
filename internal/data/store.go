f3267c67bbb3b837cc41350b074a9d8e194942b5
PiccioHub <picciohub@gmail.com>
2026-01-13 15:12:17 +0100

    Basics store operations

--- internal/data/store.go
+++ internal/data/store.go
@@ -1,6 +1,8 @@
 package data
 
 import (
+	"context"
+	"github.com/jackc/pgx/v5/pgxpool"
 	"time"
 )
 
@@ -13,4 +15,65 @@ type Store struct {
 
 type StoreInput struct {
 	Name string `json:"name" validate:"required,gte=3,lte=15"`
+}
+
+type ModelStore struct {
+	DB *pgxpool.Pool
+}
+
+func (m ModelStore) Insert(ctx context.Context, newStore StoreInput, userId string) (storeId int, err error) {
+	stmt := `INSERT INTO stores(name, user_id) 
+			 VALUES ($1, $2)
+			 RETURNING id`
+
+	args := []interface{}{newStore.Name, userId}
+
+	err = m.DB.QueryRow(ctx, stmt, args...).Scan(&storeId)
+
+	if err != nil {
+		return 0, err
+	}
+
+	return storeId, nil
+}
+
+func (m ModelStore) Get(ctx context.Context, storeId int, userId string) (store Store, err error) {
+
+	stmt := `SELECT id, name, user_id, created_at
+			 FROM stores
+			 WHERE id = $1 AND user_id = $2`
+
+	args := []interface{}{storeId, userId}
+
+	err = m.DB.QueryRow(ctx, stmt, args...).Scan(&store.ID, &store.Name, &store.UserID, &store.CreatedAt)
+
+	if err != nil {
+		return Store{}, err
+	}
+
+	return store, nil
+}
+
+func (m ModelStore) Update(ctx context.Context, newStore StoreInput, storeId int, userId string) error {
+	stmt := `UPDATE stores
+			 SET name= $1
+			 WHERE id = $2 AND user_id = $3
+			`
+
+	args := []interface{}{newStore.Name, storeId, userId}
+
+	_, err := m.DB.Exec(ctx, stmt, args)
+
+	return err
+}
+
+func (m ModelStore) Delete(ctx context.Context, storeId int, userId string) error {
+	stmt := `DELETE FROM stores
+			 WHERE id = $1 AND user_id = $2`
+
+	args := []interface{}{storeId, userId}
+
+	_, err := m.DB.Exec(ctx, stmt, args)
+
+	return err
 }
